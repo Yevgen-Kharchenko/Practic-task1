37c01e59ba9577fb8af32a8ac0c88f76e771c2f3
VLAD-KHARCHENKO <vladuslav.kharchenko.jd@gmail.com>
2020-11-26 23:52:26 +0200

    part7

--- src/main/java/com/epam/rd/java/basic/practice1/Part7.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part7.java
@@ -7,14 +7,34 @@ public class Part7 {
     }
 
     public static int str2int(String number) {
-        return 0;
+
+        int result = 0;
+        for (int i = 0; i < number.length(); i++) {
+            result *= 26;
+            result += number.charAt(i) - 'A' + 1;
+        }
+        return result;
     }
 
     public static String int2str(int number) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        while (number > 0) {
+            if (number % 26 == 0) {
+                stringBuilder.append("Z");
+                number = (number / 26) - 1;
+            } else {
+                stringBuilder.append((char) ((number % 26 - 1) + 'A'));
+                number = number / 26;
+            }
+        }
+        stringBuilder.reverse();
+        return stringBuilder.toString();
     }
 
     public static String rightColumn(String number) {
-        return null;
+        int num = str2int(number);
+        num++;
+        String string = int2str(num);
+        return string;
     }
 }
